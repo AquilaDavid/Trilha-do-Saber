d42e3c604cb62e7019c4cbf1a0d690b9a11ce4d1
AquilaDavid <aquiladavid07@gmail.com>
2026-02-14 01:58:59 -0300

    feat: criação da main

---
+++ app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import controller.FachadaDoJogo;
+
+public class Main {
+
+public static void main(String[] args) {
+        FachadaDoJogo fachada = new FachadaDoJogo();
+        fachada.iniciar();
+    }
+}
